f11df3919f93bed08eb9d337e043a815da4ad8a1
becevka <becevka@gmail.com>
2015-09-03 22:09:42 -0500

    Require support

---
+++ examples/io/resources.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var path = require('path');
+
+var resources = {};
+
+
+function isFn(v) {
+    return typeof v == 'function';
+}
+
+resources['file'] = function (source, parameters, there) {
+    var val = null;
+    var file = parameters[0];
+    if (isFn(file.value)) {
+        file = file.value();
+    }
+    if (!fs.existsSync(file)) {
+        file = path.join(there.dir, file);
+    }
+    if (source && source != there) {
+        if (typeof source === 'string' || (source.type && source.type == 'string')) {
+            var txt = source;
+            if (isFn(txt.value)) {
+                txt = txt.value();
+            }
+            fs.writeFileSync(file, txt, "utf-8");
+        } else {
+            val = fs.readFileSync(file, "utf-8");
+        }
+    } else {
+        val = fs.readFileSync(file, "utf-8");
+    }
+    if (val != null) {
+        var name = source.type || source;
+        there.properties[name] = there.create("string", val);
+    }
+    return source;
+};
+
+module.exports = resources;
---
+++ examples/io/vectors.js
@@ -0,0 +1,3 @@
+var vectors = {};
+
+module.exports = vectors;
